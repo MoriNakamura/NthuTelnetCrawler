6d7e9c1a57f351965f841b2d06e936d565d3b70f
MoriNakamura <f0923206757@yahoo.com.tw>
2016-09-22 00:59:11 +0800

    damn

--- __main__.py
+++ __main__.py
@@ -1,10 +1,12 @@
 import dataCleaner
 import nthuTelnetCrawler
 import manualDataCleaner
+import dataSelect
 
 def main():
     # dataCleaner.data_cleaner()
     # nthuTelnetCrawler.nthuTelnetCrawler()
-    manualDataCleaner.manual_data_cleaner()
+    # manualDataCleaner.manual_data_cleaner()
+    dataSelect.data_select()
 if __name__ == "__main__":
     main()
